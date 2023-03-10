5ff993c92abe73c057f9eea8f3302ea5d38dd953
Dindinha <indynha.w@gmail.com>
2023-03-10 17:14:43 -0300

    Error comment

--- fdf_error.c
+++ fdf_error.c
@@ -6,12 +6,17 @@
 /*   By: inwagner <inwagner@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 17:01:05 by inwagner          #+#    #+#             */
-/*   Updated: 2023/03/01 22:18:04 by inwagner         ###   ########.fr       */
+/*   Updated: 2023/03/10 17:13:57 by inwagner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+// TRATAMENTO DE ERRO
+/* A função "perror" imprime a mensagem referente ao último erro gravado, e a
+ * "strerror" imprime uma mensagem de erro de acordo com o número passado por
+ * parâmetro. A lista de erros está disponível no arquivo "error.md".
+ */
 void	print_error(int errn)
 {
 	if (errn == -1)
