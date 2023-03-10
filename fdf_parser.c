091b1fb475fda9d0583f004c73e395514e364ce5
Dindinha <indynha.w@gmail.com>
2023-03-10 12:34:06 -0300

    v1

---
+++ fdf_parser.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf_parser.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: inwagner <inwagner@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/21 16:09:53 by inwagner          #+#    #+#             */
+/*   Updated: 2023/03/10 11:44:50 by inwagner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void	get_info(char *strrow, t_mdata *m, int j)
+{
+	int		i;
+
+	i = 0;
+	while (i < m->col)
+	{
+		while (*strrow == ' ')
+			strrow++;
+		if (*strrow != '\n' || *strrow != '\0')
+		{
+			m->coord[i][j].coord[0] = i;
+			m->coord[i][j].coord[1] = j;
+			m->coord[i][j].coord[2] = ft_atoi_base(strrow, 10);
+			while (*strrow != ' ' && *strrow != ',' && *strrow != '\n' && *strrow != '\0')
+				strrow++;
+			if (*strrow == ',')
+			{
+				strrow = strrow + 3;
+				m->coord[i][j].color = ft_atoi_base(strrow, 16);
+				while (*strrow != ' ' && *strrow != '\n' && *strrow != '\0')
+					strrow++;
+			}
+			else
+				m->coord[i][j].color = 0;
+		}
+		i++;
+	}
+}
+
+void	populate_map(t_mdata *m, int fd)
+{
+	int		j;
+	char	*strrow;
+
+	j = 0;
+	while (j < m->row)
+	{
+		strrow = get_next_line(fd);
+		get_info(strrow, m, j);
+		j++;
+		free(strrow);
+	}
+}
+
+void	map_creator(t_mdata *m, int fd)
+{
+	int	i;
+
+	m->coord = malloc((sizeof(t_coordinates *) * m->col));
+	if (!m->coord)
+		super_free(0, m, -1);
+	i = 0;
+	while (i < m->col)
+	{
+		m->coord[i] = malloc((sizeof(t_coordinates) * m->row));
+		if (!m->coord[i])
+			super_free(i, m, -1);
+		i++;
+	}
+	populate_map(m, fd);
+}
